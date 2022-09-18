364a610206f2c459886b4355594bee82fab45a94
psbppelbayan <96633604+ppelbayan@users.noreply.github.com>
2022-09-18 12:57:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+$.getJSON('data.json',function(data) {
+    $.each(data,function(i,data){
+        $('#daftar-prestasie').append(
+            `<div class="col-md-4">
+                <div class="card m-2">
+                <div class="card-body">
+                    <a href="/Twibbon Jadi/`+data.foto+`">
+                    <div class="col-sm-8">
+                        <div class="card-body">
+                        <p class="card-text">`+data.khataman+`</p>
+                        <h5 class="card-title">`+data.nama+`</h5>
+                        <p class="card-text">Putra Bapak `+data.bapak+`</p>
+                        <p class="card-text">Alamat: `+data.alamat+`</p>
+                        </div>
+                    </div>
+                    </a>    
+                </div>
+                </div>
+               
+                </div>`
+        );
+    });
+});
+
+
+
+$('.nav-link').on('click',function(){
+
+    $('.nav-link').removeClass('active');
+    $(this).addClass('actives');
+
+    let kategori=$(this).html();
+    console.log(kategori)
+    $('h2').html(kategori);
+    
+    let content='';
+
+    $.getJSON('data.json', function(data){
+        $.each(data, function(i,data ){
+            if(data.khataman == kategori){
+                content += `<div class="col-md-4">
+                <div class="card m-2">
+                <div class="card-body">
+                    <a href="/Twibbon Jadi/`+data.foto+`">
+                    <div class="col-sm-8">
+                        <div class="card-body">
+                        <p class="card-text">`+data.khataman+`</p>
+                        <h5 class="card-title">`+data.nama+`</h5>
+                        <p class="card-text">Putra Bapak `+data.bapak+`</p>
+                        <p class="card-text">Alamat: `+data.alamat+`</p>
+                        </div>
+                    </div>
+                    </a>    
+                </div>
+                </div>
+               
+                </div>`;
+            }
+          
+        });
+        $('#daftar-prestasie').html(content);
+    });
+});
